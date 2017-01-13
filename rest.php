db873cefba77060312681d4eeb74e12fffac935a
mikeg <mike.griffin@rightmove.co.uk>
2017-01-13 14:50:22 +0000

    no message

--- rest.php
+++ rest.php
@@ -1,16 +1,23 @@
 <?php
  header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE');
  header("Access-Control-Allow-Origin: *");
+ $method = $_SERVER['REQUEST_METHOD'];
 include 'sqlconnect.php';
 
-$sql = "SELECT * FROM gpsname";
-$result = $conn->query($sql);
+if ($method=="GET"){
+  $sql = "SELECT * FROM gpsname";
+  $result = $conn->query($sql);
 
-if ($result) {
-    while($row = $result->fetch_array(MYSQL_ASSOC)) {
-            $myArray[] = $row;
-    }
-    echo json_encode($myArray);
+  if ($result) {
+      while($row = $result->fetch_array(MYSQL_ASSOC)) {
+              $myArray[] = $row;
+      }
+      echo json_encode($myArray);
+  }
+}
+
+if ($method=="POST"){
+  echo json_encode("post");
 }
 
 $conn->close();
