aa685a5356a3f0ed93fec6ac09132b9c8d0b1aca
mikeg <mike.griffin@rightmove.co.uk>
2017-01-13 14:12:23 +0000

    no message

--- rest.php
+++ rest.php
@@ -1,6 +1,6 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- header("Access-Control-Allow-Methods: *");
+ header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE');
 include 'sqlconnect.php';
 
 $sql = "SELECT * FROM gpsname";
